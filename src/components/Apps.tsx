5fe8e18d36197e2fc33c451615cd74190dc84608
mojtaba-esk <mojtaba.esk@gmail.com>
2020-03-19 09:33:47 +0100

    App Manager

--- src/components/Apps.tsx
+++ src/components/Apps.tsx
@@ -1,12 +1,116 @@
 import * as React from "react";
+import * as waziup from "waziup";
+import App from "./apps/App";
+import NewApp from "./apps/NewApp";
+import {
+  MDBIcon,
+  MDBAlert,
+  MDBCol,
+  MDBContainer,
+  MDBRow,
+  MDBBtn
+} from "mdbreact";
 
-export class AppsPageComp extends React.Component {
+declare var gateway: waziup.Waziup;
 
-    constructor(props: {}) {
-        super(props);
+export interface Props {
+  filter?: "installed" | "available";
+}
+
+export interface State {
+  apps: waziup.App[];
+  loading: boolean;
+}
+
+export class AppsPageComp extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      apps: null,
+      loading: true
+    };
+  }
+
+  /*---------------*/
+
+  componentDidMount() {
+    this.load();
+  }
+
+  /*---------------*/
+
+  load() {
+    this.setState({ loading: true });
+    gateway.getApps().then(
+      res => {
+        this.setState({
+          apps: res,
+          loading: false
+        });
+      },
+      error => {
+        // Notify(error);
+        this.setState({ loading: false });
+      }
+    );
+  }
+
+  /*---------------*/
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <div className="center p-lg-5">
+          Loading <MDBIcon spin icon="cog" />
+        </div>
+      );
     }
 
-    render() {
-        return <div>Apps :D</div>
+    /*-------*/
+
+    if (!this.state.apps) {
+      return (
+        <MDBAlert color="info" className="m-3">
+          <MDBIcon icon="exclamation-circle" />
+          <span className=""> There are no Apps installed.</span>
+        </MDBAlert>
+      );
     }
+
+    /*-------*/
+    var results;
+    if (this.props.filter == "available") {
+      results = this.state.apps
+        ? this.state.apps.map((res, index) => (
+            <MDBCol key={index} sm="4">
+              <NewApp id={res.id} />
+            </MDBCol>
+          ))
+        : "";
+    } else {
+      results = this.state.apps
+        ? this.state.apps.map((res, index) => (
+            <MDBCol key={index} sm="4">
+              <App id={res.id} />
+            </MDBCol>
+          ))
+        : "";
+    }
+
+    return (
+      <MDBContainer>
+        <MDBRow>
+          <MDBBtn
+            style={{ display: this.props.filter == "available" ? "" : "none" }}
+          >
+            <a href="/#/apps/new">
+              <MDBIcon icon="puzzle-piece" /> Install a new App
+            </a>
+          </MDBBtn>
+        </MDBRow>
+        <MDBRow>{results}</MDBRow>
+      </MDBContainer>
+    );
+  }
 }
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -8,174 +8,205 @@ import * as waziup from "waziup";
 import { AppsProxyComp } from "./AppsProxy";
 import { MetaHandler, metaHandler } from "./devices/Entity";
 
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+
 declare var gateway: waziup.Waziup;
 
 type MenuItem = {
-    icon: string;
-    label: string;
-    href: string;
-    items?: MenuItem[];
-    target?: string;
-}
+  icon: string;
+  label: string;
+  href: string;
+  items?: MenuItem[];
+  target?: string;
+};
 
 interface State {
-    page: string;
-    loading: {
-        pending: number;
-    }
+  page: string;
+  loading: {
+    pending: number;
+  };
 }
 
 const appsRegExp = /^#\/apps\/([a-zA-Z0-9_-]+)\/(.+)/;
 
 var menu = (item: MenuItem) => {
-    return (
-        <a key={`${item.label}${item.href}`}className="menu-item" href={item.href} target={item.target||"_self"}>
-            <IconComp className="item-icon" src={item.icon} />
-            <span className="item-name">{item.label}</span>
-        </a>
-    );
-}
+  return (
+    <a
+      key={`${item.label}${item.href}`}
+      className="menu-item"
+      href={item.href}
+      target={item.target || "_self"}
+    >
+      <IconComp className="item-icon" src={item.icon} />
+      <span className="item-name">{item.label}</span>
+    </a>
+  );
+};
 
 type WazigatePkg = {
-    menu: MenuItem[];
-    hook: string;
-}
-
+  menu: MenuItem[];
+  hook: string;
+};
 
 export class DashboardComp extends React.Component<{}, State> {
+  menu: MenuItem[] = [
+    {
+      label: "Dashboard",
+      icon: `dist/${icons}#dashboard`,
+      href: "#/",
+      items: []
+    },
+    {
+      label: "Synchronization",
+      icon: `dist/${icons}#clouds`,
+      href: "#/clouds",
+      items: []
+    },
+    {
+      label: "Apps",
+      icon: `dist/${icons}#apps`,
+      href: "#/apps",
+      items: []
+    }
+  ];
 
-    menu: MenuItem[] = [{
-        label: "Dashboard",
-        icon: `dist/${icons}#dashboard`,
-        href: "#/",
-        items: []
-    }, {
-        label: "Synchronization",
-        icon: `dist/${icons}#clouds`,
-        href: "#/clouds",
-        items: []
-    }];
+  constructor(props: {}) {
+    super(props);
 
-    constructor(props: {}) {
-        super(props);
-
-        this.onHandleHashChange = this.onHandleHashChange.bind(this);
-        var page = location.hash;
-        if (page.length < 2) {
-            location.hash = page = "#/";
-        }
-
-        this.state = {
-            page,
-            loading: {
-                pending: 0
-            },
-        }
-
-        window.addEventListener("hashchange", this.onHandleHashChange, false);
-
-        this.load();
+    this.onHandleHashChange = this.onHandleHashChange.bind(this);
+    var page = location.hash;
+    if (page.length < 2) {
+      location.hash = page = "#/";
     }
 
-    async load() {
-        (window as any)["dashboard"] = {
-            dashboard: this
-        }
+    this.state = {
+      page,
+      loading: {
+        pending: 0
+      }
+    };
 
-        var apps = await gateway.getApps();
-        var pending = apps.length;
-        if(pending === 0) {
-            this.setState({loading: null});
-            return
-        }
-        this.setState({
-            loading: { pending }
-        });
-        apps.forEach(async app => {
-            var file = await gateway.fetch(gateway.toProxyURL(app.name, "package.json"), {})
-            var pkg = await file.json();
-            var wazigatePkg = pkg["wazigate"] as WazigatePkg;
-            if (wazigatePkg.menu) {
-                this.menu.push(...wazigatePkg.menu);
-            }
-            if(wazigatePkg.hook) {
-                loadHook(app.name, wazigatePkg.hook);
-            } else {
-                this.completeHook();
-            }
-        });
+    window.addEventListener("hashchange", this.onHandleHashChange, false);
+
+    this.load();
+  }
+
+  async load() {
+    (window as any)["dashboard"] = {
+      dashboard: this
+    };
+
+    var apps = await gateway.getApps();
+    var pending = apps ? apps.length : 0;
+    if (pending === 0) {
+      this.setState({ loading: null });
+      return;
+    }
+    this.setState({
+      loading: { pending }
+    });
+    apps.forEach(async app => {
+      var appInfo = (await gateway.getApp(app.id)) as any;
+
+      if (
+        !appInfo ||
+        Object.getOwnPropertyNames(appInfo).length == 0 ||
+        !appInfo.package ||
+        Object.getOwnPropertyNames(appInfo.package).length == 0
+      ) {
+        var pending = this.state.loading.pending - 1;
+        this.setState({ loading: { pending } });
+        if (pending <= 0) this.completeHook();
+        return;
+      }
+      var wazigatePkg = (await appInfo.package) as WazigatePkg;
+      if (wazigatePkg.menu) {
+        this.menu.push(...wazigatePkg.menu);
+      }
+      if (wazigatePkg.hook) {
+        loadHook(app.id, wazigatePkg.hook);
+      } else {
+        this.completeHook();
+      }
+    });
+  }
+
+  addMetaHandler(meta: string, handler: MetaHandler) {
+    if (meta in metaHandler) {
+      metaHandler[meta].push(handler);
+    } else {
+      metaHandler[meta] = [handler];
+    }
+  }
+
+  getMetaHandler(meta: string) {
+    return metaHandler[meta] || [];
+  }
+
+  completeHook() {
+    if (--this.state.loading.pending <= 0) {
+      this.setState({ loading: null });
+    }
+  }
+
+  onHandleHashChange() {
+    this.navigate(location.hash);
+  }
+
+  navigate(page: string) {
+    this.setState({ page });
+  }
+
+  render() {
+    var page = this.state.page;
+    var match: RegExpMatchArray;
+
+    if (this.state.loading) {
+      return (
+        <div className="loading">
+          Loading, please wait. Pending: {this.state.loading.pending} apps.
+        </div>
+      );
     }
 
-    addMetaHandler(meta: string, handler: MetaHandler) {
-        if(meta in metaHandler) {
-            metaHandler[meta].push(handler);
-        } else {
-            metaHandler[meta] = [handler];
-        }
+    if (page == "#/") {
+      var pageComp = <DevicesPageComp />;
+    } else if (page === "#/apps") {
+      var pageComp = <AppsPageComp filter="installed" />;
+    } else if (page === "#/apps/new") {
+      var pageComp = <AppsPageComp filter="available" />;
+    } else if ((match = page.match(appsRegExp))) {
+      var pageComp = <AppsProxyComp app={match[1]} path={match[2]} />;
+    } else {
+      var pageComp = (
+        <ErrorComp
+          error={`Page not found.\nThere is nothing at \"${page}\".`}
+        />
+      );
     }
 
-    getMetaHandler(meta: string) {
-        return metaHandler[meta] || [];
-    }
-
-    completeHook() {
-        if(--this.state.loading.pending === 0) {
-            this.setState({loading: null});
-        }
-    }
-
-    onHandleHashChange() {
-        this.navigate(location.hash);
-    }
-
-    navigate(page: string) {
-        this.setState({ page });
-    }
-
-    render() {
-        var page = this.state.page;
-        var match: RegExpMatchArray;
-
-        if (this.state.loading) {
-            return <div className='loading'>Loading, please wait. Pending: {this.state.loading.pending} apps.</div>
-        }
-
-        if (page == "#/") {
-            var pageComp = <DevicesPageComp />;
-
-        } else if (page === "#/apps") {
-            var pageComp = <AppsPageComp />;
-
-        }  else if ((match = page.match(appsRegExp))) {
-            var pageComp = <AppsProxyComp app={match[1]} path={match[2]} />;
-
-        } else {
-            var pageComp = <ErrorComp error={`Page not found.\nThere is nothing at \"${page}\".`} />;
-        }
-
-        return (
-            <div className="dashboard">
-                <div id="menu">
-                    {this.menu.map(menu)}
-                </div>
-                <div id="statusbar"></div>
-                {pageComp}
-            </div>
-        )
-    }
+    return (
+      <div className="dashboard">
+        <div id="menu">{this.menu.map(menu)}</div>
+        <div id="statusbar"></div>
+        {pageComp}
+      </div>
+    );
+  }
 }
 
-
 function loadHook(app: string, hook: string) {
-    return new Promise((resolve, reject) => {
-        var script = document.createElement("script");
-        // var rnd = `${Math.random()}`.slice(2);
-        // hooks[rnd] = () => {
-        //     delete hooks[rnd];
-        //     resolve();
-        // }
-        // script.setAttribute("data-hook", rnd);
-        script.src = gateway.toProxyURL(app, hook);
-        document.head.appendChild(script);
-    });
+  return new Promise((resolve, reject) => {
+    var script = document.createElement("script");
+    // var rnd = `${Math.random()}`.slice(2);
+    // hooks[rnd] = () => {
+    //     delete hooks[rnd];
+    //     resolve();
+    // }
+    // script.setAttribute("data-hook", rnd);
+    script.src = gateway.toProxyURL(app, hook);
+    document.head.appendChild(script);
+  });
 }
