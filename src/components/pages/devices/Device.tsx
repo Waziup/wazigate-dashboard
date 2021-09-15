472c6e823596c7353aa8cd2c9cf9e7664901e764
Johann Forster <25847959+j-forster@users.noreply.github.com>
2021-09-15 11:15:20 +0200

    Bug fix
    Fixes https://github.com/Waziup/WaziGate/issues/163.

--- src/components/pages/devices/Device.tsx
+++ src/components/pages/devices/Device.tsx
@@ -180,7 +180,7 @@ export const DeviceComp = ({ device, className, isGateway, onDelete }: Props) =>
                         <Avatar
                             aria-label="recipe"
                             className={classes.avatar}
-                            style={{ background: colorFromRune(deviceName[0]) }}
+                            style={{ background: colorFromRune(device.id[0]) }}
                         >
                             {deviceName[0]}
                         </Avatar>
@@ -223,14 +223,14 @@ export const DeviceComp = ({ device, className, isGateway, onDelete }: Props) =>
                         </ListItemIcon>
                         <ListItemText primary="Change Gateway ID" />
                     </MenuItem>)
-                : (
-                <MenuItem onClick={handleDeleteClick}>
-                    <ListItemIcon>
-                        <DeleteIcon fontSize="small" />
-                    </ListItemIcon>
-                    <ListItemText primary="Delete" />
-                </MenuItem>)}
-                
+                    : (
+                        <MenuItem onClick={handleDeleteClick}>
+                            <ListItemIcon>
+                                <DeleteIcon fontSize="small" />
+                            </ListItemIcon>
+                            <ListItemText primary="Delete" />
+                        </MenuItem>)}
+
                 {/* <Divider />
                 <MenuItem onClick={handleNameClick}>
                     <ListItemIcon>
@@ -312,14 +312,16 @@ export const DeviceComp = ({ device, className, isGateway, onDelete }: Props) =>
     );
 }
 
-const alphabetcolors = ["#5A8770", "#B2B7BB", "#6FA9AB", "#F5AF29", "#0088B9", "#F18636", "#D93A37", "#A6B12E", "#5C9BBC", "#F5888D", "#9A89B5", "#407887", "#9A89B5", "#5A8770", "#D33F33", "#A2B01F", "#F0B126", "#0087BF", "#F18636", "#0087BF", "#B2B7BB", "#72ACAE", "#9C8AB4", "#5A8770", "#EEB424", "#407887"];
+const alphabetColors = [
+    "#5A8770", "#B2B7BB", "#6FA9AB", "#F5AF29", "#0088B9", "#F18636", "#D93A37",
+    "#A6B12E", "#5C9BBC", "#F5888D", "#9A89B5", "#407887", "#9A89B5", "#5A8770",
+    "#D33F33", "#A2B01F", "#F0B126", "#0087BF", "#F18636", "#0087BF", "#B2B7BB",
+    "#72ACAE", "#9C8AB4", "#5A8770", "#EEB424", "#407887"];
 
 function colorFromRune(r: string): string {
-    if (r.charCodeAt(0) < 65) {
-        return randomColor();
-    }
-    const colorIndex = Math.floor((r.charCodeAt(0) - 65) % alphabetcolors.length);
-    return alphabetcolors[colorIndex];
+    if (!r) return randomColor();
+    const colorIndex = Math.floor(r.charCodeAt(0) % alphabetColors.length);
+    return alphabetColors[colorIndex];
 }
 
 function randomColor(): string {
