c013185f2037a37b78fd4e6cb32cafafe438f4eb
Felix Markwordt <24inchstreetbikesoldier@gmail.com>
2022-07-25 15:20:59 +0200

    added full date to csv-file

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -7,7 +7,8 @@ import {
     makeStyles,
 } from '@material-ui/core';
 import { ValueWithTime } from 'waziup';
-import { dateFormatter, formatValue } from '../../tools';
+import { dateFormatter, formatValue} from '../../tools';
+import { csv } from 'd3';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -34,32 +35,9 @@ type Props = {
 };
 
 type DataPoint = {
-    x: Date;
-    y: number;
+  x: Date;
+  y: number;
 };
-
-function validPoint(p: DataPoint) {
-    return  typeof p.y === "boolean" ||  typeof p.y === "number";
-}
-
-function displayPoint(p: DataPoint): DataPoint {
-    switch(typeof p.y) {
-        case "number":
-            return p;
-        case "boolean":
-            return {
-                x: p.x,
-                y: p.y ? 1 : 0
-            }
-        default:
-            // unreached
-            throw "no a supported value";
-    }
-}
-
-function checkValidValues(points: DataPoint[]) {
-    return points.filter(validPoint).map(displayPoint);
-}
 
 
 export default function _Chart(props: Props) {
@@ -68,7 +46,16 @@ export default function _Chart(props: Props) {
 
     const options: ApexOptions = {
         chart: {
-            id: "chart1"
+            id: "chart1",
+            toolbar: {
+                export: {
+                    csv: {
+                        dateFormatter(timestamp) {
+                            return new Date(timestamp)
+                        }
+                    }
+                }
+            }
         },
         title: {
             text: props.title,
@@ -96,18 +83,23 @@ export default function _Chart(props: Props) {
     };
     const series = [{
         name: "",
-        data: checkValidValues(props.data)
+        data: props.data
     }];
 
     return (
-        <div className={classes.root}>
-            <Chart
-                className="chart" //{props.className}
-                options={options}
-                series={series}
-                type="line"
-                align='center'
-            />
+        <div>
+            <div>
+                <div className={`${classes.root}`}>
+                    <Chart
+                        className="chart" //{props.className}
+                        options={options}
+                        series={series}
+                        type="line"
+                        align='center'
+                    />
+                </div>
+            </div>
         </div>
     );
+
 }
